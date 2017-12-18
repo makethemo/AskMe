65b6cb7ed7bd0cb927a6fbbe9f52065a331bfcaf
lee930402 <chive0402@naver.com>
2017-12-18 14:22:14 +0900

    main : body를 파일로 저장하지 않고 바로 파싱할 수 있도록 수정

--- vision/vision_api_request.py
+++ vision/vision_api_request.py
@@ -94,20 +94,21 @@ if __name__ == '__main__':
     local_image_path = "./orange.jpg"    # You have to fix the image path here.
     (headers, body) = get_response(encode_image(local_image_path, 'ascii'))
 
-    # 이렇게 하면 파일을 따로 저장해야하고
+    """ 이렇게 하면 파일을 따로 저장해야하고
     f = open("./test.json", 'w')
     f.write(body.decode('utf-8'))
     f.close()
 
     print(image_label_detection("./test.json"))
-    """
+    
     with open('./test.json') as data_file:
         data = json.load(data_file)
     size = len(data["responses"][0]["labelAnnotations"])
     print(data["responses"][0]["webDetection"]["webEntities"][0]["description"])  # 값 하나하나 접근하기
     """
 
-    """ 이렇게 하면 문자 하나하나씩 접근해야돼요
-    str = body.decode('utf-8')
-    print(str)
-    """
+    #""" # 이렇게 하면 문자 하나하나씩 접근해야돼요
+    # str = body.decode('utf-8')
+    data = json.load(body.decode('utf-8'))
+    print(data["responses"][0]["labelAnnotations"])
+    #"""
