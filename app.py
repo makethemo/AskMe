ddaed5da726254ea56a098aa07858a049405492b
Jinseop Mo <makethemo@gmail.com>
2017-12-22 04:41:18 +0900

    Add TTS_DIR path

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+TTS_DIR = os.path.join(os.path.join(ROOT_DIR, 'speech'), '.tts')
 PICTURES_DIR = os.path.join(ROOT_DIR, '.pictures')
 KEY_PATH = os.path.join(ROOT_DIR, 'key.json')
 
