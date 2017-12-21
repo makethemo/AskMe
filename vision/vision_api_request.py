d878202f325f83efcf9d75154fc670ad5c97f3c2
Jinseop Mo <makethemo@gmail.com>
2017-12-21 22:30:07 +0900

    Remove redundant parentheses

--- vision/vision_api_request.py
+++ vision/vision_api_request.py
@@ -57,26 +57,26 @@ def image_label_detection(data):
     length = len(data["responses"][0]["labelAnnotations"])
 
     for i in range(length):  # label과 그에 대응하는 score 저장
-        if(data["responses"][0]["labelAnnotations"][i]["score"] > 0.6):
+        if data["responses"][0]["labelAnnotations"][i]["score"] > 0.6:
             label.append(data["responses"][0]["labelAnnotations"][i]["description"])
             score.append(data["responses"][0]["labelAnnotations"][i]["score"])
-            if(label[len(label)-1] == "product" or label[len(label)-1] == "produce"):
+            if label[len(label)-1] == "product" or label[len(label)-1] == "produce":
                 label.pop()
                 label.append(data["responses"][0]["webDetection"]["webEntities"][0]["description"])
 
     length = len(label)
 
     # 조건에 맞게 label 잘라내기
-    if(score[0] > 0.95):
-        while(score[length-1] < 0.95):
+    if score[0] > 0.95:
+        while score[length-1] < 0.95:
             label.pop()
             length = len(label)
-    elif(sum(score)/length > 0.75):
-        while(score[length-1] < 80):
+    elif sum(score)/length > 0.75:
+        while score[length-1] < 80:
             label.pop()
             length = len(label)
     else:
-        while(score[length -1] < 70):
+        while score[length-1] < 70:
             label.pop()
             length = len(label)
 
