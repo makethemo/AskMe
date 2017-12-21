4f0c8597d3465f223e58dd9bd0a35632006eebc8
Jinseop Mo <makethemo@gmail.com>
2017-12-21 22:46:11 +0900

    Refactor camera/camera.py

--- camera/camera.py
+++ camera/camera.py
@@ -5,30 +5,33 @@ import os
 
 import app
 
+camera = picamera.PiCamera()
+camera.vflip = True
+camera.exposure_mode = 'auto'  # 노출
+camera.resolution = (1920, 1080)  # 해상도
 
-def picture():
 
-    camera = picamera.PiCamera()
-    camera.vflip = True
-    camera.exposure_mode = 'auto'  # 노출
-    t = datetime.datetime.now().strftime('%Y%m%d_%H%M_%S')  # 현재 시간
-    camera.resolution = (1920, 1080)  # 해상도
+def get_time_for_filename():
+    return datetime.datetime.now().strftime('%Y%m%d_%H%M_%S')  # 현재 시간
+
+
+def start_preview():
     camera.start_preview()
-    sleep(1)
-    file_name = t + '.jpg'
+
+
+def stop_preview():
+    camera.stop_preview()
+
+
+def take_a_picture():
+    file_name = get_time_for_filename() + '.jpg'
     camera.capture(os.path.join(app.PICTURES_DIR, file_name))  # 시스템 시간
     return file_name
 
 
-def video():
-
-    camera = picamera.PiCamera()
-    camera.vflip = True
-    camera.exposure_mode = 'auto'  # 노출
-    t = datetime.datetime.now().strftime('%Y%m%d_%H%M_%S')  # 현재 시간
-    camera.resolution = (1920, 1080)
-    file_name = t + '.h264'
+def take_a_video(seconds):
+    file_name = get_time_for_filename() + '.h264'
     camera.start_recording(os.path.join(app.PICTURES_DIR, file_name))
-    sleep(10)
+    sleep(seconds)
     camera.stop_recording()
     return file_name
