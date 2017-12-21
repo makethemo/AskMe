251bdaee7d7e24ba334dc4ad73ce892a112788f0
Jinseop Mo <makethemo@gmail.com>
2017-12-22 06:30:53 +0900

    Implement RPi button

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from time import sleep
+import RPi.GPIO as GPIO
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -7,4 +9,20 @@ KEY_PATH = os.path.join(ROOT_DIR, 'key.json')
 
 
 if __name__ == '__main__':
-    pass
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(24, GPIO.OUT)
+    
+    try:
+        while True:
+            button_state = GPIO.input(18)
+            if button_state == False:
+                GPIO.output(24, True)  # LED on
+                # print('Button pressed...')
+                # TODO: implement record
+                sleep(0.2)
+            else:
+                GPIO.output(24, False)  # LED off
+    except:
+        GPIO.cleanup()
+
