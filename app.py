8381df9c635c2f081da1f59f3778c77e2fa5ab81
Jinseop Mo <makethemo@gmail.com>
2017-12-22 07:35:31 +0900

    Add recorder

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ from time import sleep
 import RPi.GPIO as GPIO
 import os
 
+from speech.recorder import get_recorder
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 TTS_DIR = os.path.join(os.path.join(ROOT_DIR, 'speech'), '.tts')
+MIC_DIR = os.path.join(ROOT_DIR, '.mic')
 PICTURES_DIR = os.path.join(ROOT_DIR, '.pictures')
 KEY_PATH = os.path.join(ROOT_DIR, 'key.json')
+
+
+def new_filename():
+    import datetime
+    return datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
 
 if __name__ == '__main__':
@@ -13,16 +21,25 @@ if __name__ == '__main__':
     GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(24, GPIO.OUT)
     
+    rec = get_recorder()
+    rec.init()
+    
+    # TODO: time count
+
     try:
         while True:
             button_state = GPIO.input(18)
             if button_state == False:
                 GPIO.output(24, True)  # LED on
                 # print('Button pressed...')
-                # TODO: implement record
+                rec.start_recording()
                 sleep(0.2)
             else:
                 GPIO.output(24, False)  # LED off
+                rec.stop_recording()
+                filename = new_filename()
+                rec.save(filename)
+                # TODO call dialogflow
     except:
         GPIO.cleanup()
 
