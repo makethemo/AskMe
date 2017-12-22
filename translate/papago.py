066b82c639b472b820cce9e6598cef980351a1bc
ruddbs203 <34668393+ruddbs203@users.noreply.github.com>
2017-12-22 11:32:43 +0900

    Add translate/papago.py

---
+++ translate/papago.py
@@ -0,0 +1,26 @@
+import urllib.request
+
+with open('./key.json', 'r') as key:
+    client_id = key['papago_client_id']
+    client_secret = key['papago_client_secret']
+
+encText = urllib.parse.quote("Tree")
+data = "source=en&target=ko&text=" + encText
+url = "https://openapi.naver.com/v1/language/translate"
+
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id", client_id)
+request.add_header("X-Naver-Client-Secret", client_secret)
+response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+rescode = response.getcode()
+
+
+def english_to_korean(encText):
+    print("번역된 한글입니다 : %s" %encText)
+    if encText:
+        print ("나무")
+
+    else:
+        print("Error Code:" + rescode)
+
+    return
