48d696601c9557411df68ceb9fed0931659dd24e
Hyeonwoo <khwoo2054@gmail.com>
2017-12-28 00:20:39 +0900

    Add functional description : camera.py

--- camera/camera.py
+++ camera/camera.py
@@ -6,31 +6,31 @@ import os
 import path
 
 camera = picamera.PiCamera()
-camera.vflip = True
+camera.vflip = True  # 카메라 상하 반전
 camera.exposure_mode = 'auto'  # 노출
 camera.resolution = (1920, 1080)  # 해상도
 
 
-def get_time_for_filename():
+def get_time_for_filename():  # 현재시간을 파일 이름으로
     return datetime.datetime.now().strftime('%Y%m%d_%H%M_%S')  # 현재 시간
 
 
-def start_preview():
+def start_preview():  # 미리보기 시작
     camera.start_preview()
 
 
-def stop_preview():
+def stop_preview():  # 미리보기 중지
     camera.stop_preview()
 
 
-def take_a_picture():
+def take_a_picture():  # 사진찍기
     file_name = get_time_for_filename() + '.jpg'
     file_path = os.path.join(path.PICTURES_DIR, file_name)
     camera.capture(file_path)  # 시스템 시간
     return file_path
 
 
-def take_a_video(seconds):
+def take_a_video(seconds):  # 비디오 찍기
     file_name = get_time_for_filename() + '.h264'
     file_path = os.path.join(path.PICTURES_DIR, file_name)
     camera.start_recording(file_path)
